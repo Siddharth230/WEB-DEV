33574d6b285541ba006fc8811be3585d793af8c6
Siddharth230 <siddharthlokhande04@gmail.com>
2024-09-13 19:45:04 +0530

    WEEK 2

--- WEEK 2/index.js
+++ WEEK 2/index.js
@@ -20,6 +20,7 @@ const ans = sum(100);
 console.log(ans);
 */
 
+/*
 function sum(n) {
 	let ans = 0;
 	for (let i = 1; i <= n; i++) {
@@ -34,3 +35,54 @@ const ans2 = sum(1000);
 console.log(ans2);
 const ans3 = sum(10000);
 console.log(ans3);
+*/
+
+/*
+const fs = require("fs");
+
+const contents = fs.readFileSync("WEEK 2/a.txt", "utf8");
+console.log(contents);
+
+const contents1 = fs.readFileSync("WEEK 2/assignment.txt", "utf8");
+console.log(contents1);
+*/
+
+/*
+const fs = require("fs");
+
+fs.readFile("WEEK 2/a.txt", "utf8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("WEEK 2/assignment.txt", "utf8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("WEEK 2/b.txt", "utf8", function (err, contents) {
+  console.log(contents);
+});
+*/
+
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function doOperation(a, b, op) {
+  let val = op(a, b);
+  return val;
+}
+
+const ans = doOperation(1, 2, divide);
+console.log(ans);
