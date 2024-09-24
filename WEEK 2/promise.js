54fecd67be523368e3124c97b6d6ad30966a1cdf
Siddharth230 <siddharthlokhande04@gmail.com>
2024-09-24 09:03:11 +0530

    WEEK 2

--- WEEK 2/promise.js
+++ WEEK 2/promise.js
@@ -1,10 +1,45 @@
-function setTimeoutPromisified(ms) {
-  let p = new Promise((resolve) => setTimeout(resolve, ms));
-  return p;
+// function waitFor3S(resolve) {
+//   setTimeout(resolve, 10000);
+// }
+
+// function setTimeoutPromisified() {
+//   return new Promise(waitFor3S);
+// }
+
+// function main() {
+//   console.log("main is called");
+// }
+
+// setTimeoutPromisified().then(main);
+
+function random(resolve) {
+  setTimeout(resolve, 10000); //resolve is also a function
 }
 
+let p = new Promise(random); //supposed to return u something eventually
+
+//using the eventual value returned by the promise
 function callback() {
-  console.log("3 seconds have passed");
+  console.log("promise succeded");
 }
 
-setTimeoutPromisified(3000).then(callback);
+p.then(callback);
+
+const fs = require("fs");
+function cleanFile(filePath, cb) {
+  return new Promise(function (resolve) {
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      data = data.trim();
+      fs.writeFile(filePath, data, function () {
+        resolve();
+      });
+    });
+  });
+}
+
+async function main() {
+  await cleanFile("WEEK 2/a.txt");
+  console.log("Done cleaning file");
+}
+
+main();
