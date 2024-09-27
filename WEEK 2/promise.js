a52c544a74bff459291b4e41819203c663ec56dd
Siddharth230 <siddharthlokhande04@gmail.com>
2024-09-27 16:02:06 +0530

    WEEK 2

--- WEEK 2/promise.js
+++ WEEK 2/promise.js
@@ -12,6 +12,7 @@
 
 // setTimeoutPromisified().then(main);
 
+/*
 function random(resolve) {
   setTimeout(resolve, 10000); //resolve is also a function
 }
@@ -43,3 +44,55 @@ async function main() {
 }
 
 main();
+*/
+
+/*
+const fs = require("fs");
+
+function readTheFile(sendTheFinalValueHere) {
+  fs.readFile("WEEK 2/a.txt", "utf-8", function (err, data) {
+    sendTheFinalValueHere(data);
+  });
+}
+
+function readFile(fileName) {
+  //read the file and return its value
+  return new Promise(readTheFile);
+}
+
+const p = readFile();
+
+function callback(contents) {
+  console.log(contents);
+}
+
+p.then(callback);
+*/
+
+const fs = require("fs");
+
+console.log("--------top of the file--------");
+
+function readTheFile(resolve) {
+  console.log("readTheFile called");
+  setTimeout(function () {
+    console.log("callback based setTimeout completed");
+    resolve();
+  }, 3000);
+}
+
+function setTimeoutPromisified(fileName) {
+  console.log("setTimeoutPromisified called");
+  //read the file and return its value
+  return new Promise(readTheFile);
+}
+
+const p = setTimeoutPromisified();
+
+function callback() {
+  console.log("timer is done");
+}
+
+p.then(callback);
+
+console.log("-----end of the file-----");
