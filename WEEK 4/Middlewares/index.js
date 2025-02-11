d2934f26d0be30e44376724f5d3cc089934305da
Siddhath230 <siddharthlokhande04@gmail.com>
2025-02-11 10:11:37 +0530

    WEEK 4

--- WEEK 4/Middlewares/index.js
+++ WEEK 4/Middlewares/index.js
@@ -4,46 +4,29 @@ const app = express();
 
 // function that returns a boolean if the age of th person is more than 14
 
-function isOldEnough(age) {
-  if (age >= 14) {
-    return true;  
-  } else {
-    return false;
-  }
-}
-
 function isOldEnoughMiddleware(req, res, next) {
+  const age = req.query.age;
   if (age >= 14) {
     next();
   } else {
     res.json({
       msg:"Sorry you are not of age yet ",
-    })
+    });
   }
 }
 
+app.use(isOldEnoughMiddleware); //only triggers all the endpoints below it
+
 app.get("/ride2", function (req, res) {
-  if (isOldEnough(req.query.age)){
     res.json({
-      msg:"You have successfully riden the ride 2."
-    })
-  } else {
-    res.status(411).json({
-      msg:"Sorry you are not of age yet."
-    })
-  }
-})
+      msg:"You have successfully ridden the ride 2."
+    });
+});
 
 app.get("/ride1", function (req, res) {
-  if (isOldEnough(req.query.age)){
     res.json({
-      msg:"You have successfully riden the ride 1."
-    })
-  } else {
-    res.status(411).json({
-      msg:"Sorry you are not of age yet."
-    })
-  }
-})
+      msg:"You have successfully ridden the ride 1."
+    });
+});
 
 app.listen(3000);
