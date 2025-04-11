c5dd4bb08ba10f08d8ba479d21125c7643223781
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-11 08:20:31 +0530

    React useEffect

--- WEEK 9/React UseState/React-Learning/src/App.jsx
+++ WEEK 9/React UseState/React-Learning/src/App.jsx
@@ -1,29 +1,45 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  let [counterVisible, setCounterVisible] = useState(true);
+  const [count, setCount] = useState(0);
+
+  useEffect(function() {
+    setInterval(function() {
+      setCounterVisible(c => !c)
+      let clock = setInterval(function() {
+        // setCount(count => count + 1);
+        console.log("from inside the setInterval");
+        setCount(c => c + 1)
+      }, 1000);
+  
+      return  function() {
+        console.log("on unmount")
+        clearInterval(clock)
+      }
+    }, 5000)
+  }, [])
+
   return <div>
-    <Counter></Counter>
+    hi  
+    <Counter count={count}></Counter> 
+    <br/>hello
   </div>
 }
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  console.log("counter");
 
   // Guard our setInterval from re-renders
-  useEffect(function() {
-    setInterval(function() {
-      // setCount(count => count + 1);
-      setCount(function(count) {
-        return count + 1;
-      })
-    }, 1000)
+  useEffect(function(props) {
+    console.log("on mount");
   }, [])
 
   function increaseCount() {
     setCount(count + 1);
   }
   return <div>
-    <h1>{count}</h1>
+    <h1>{props.count}</h1>
     <button onClick={increaseCount}>Increase count</button>
   </div>
 }
