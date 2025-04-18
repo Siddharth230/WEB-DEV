e9f8e724a02f52a8b769cd896a30dca86e0ab571
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-18 09:25:13 +0530

    Error boundary

--- WEEK 9/Recorded/React From Basics 2/src/App.jsx
+++ WEEK 9/Recorded/React From Basics 2/src/App.jsx
@@ -1,30 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const todos = [
-    {
-      id: 1,
-      title: "Go to gym",
-      done: false,
-    },
-    {
-      id: 2,
-      title: "Eat food",
-      done: true,
-    },
-  ];
-
-  const todosComponent = todos.map((todo) => (
-    <Todo key={todo.id} title={todo.title} done={todo.done} />
-  ));
-
-  return <div>{todosComponent}</div>;
+  return (
+    <div>
+      <ErrorBoundary>
+        <Card1 />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Card2 />
+      </ErrorBoundary>
+    </div>
+  );
 }
 
-function Todo({ id, title, done }) {
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("Error caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            backgroundColor: "red",
+            borderRadius: 20,
+            padding: 20,
+          }}>
+          Something went wrong
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+function Card1() {
+  // throw new Error("Error while rendering");
+
   return (
-    <div key={id}>
-      {title} - {done ? "Done!" : "Not done"}
+    <div
+      style={{
+        backgroundColor: "red",
+        borderRadius: 20,
+        padding: 20,
+        marginBottom: 20,
+      }}>
+      Hi there
+    </div>
+  );
+}
+
+function Card2() {
+  // throw new Error("Error while rendering");
+
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        borderRadius: 20,
+        padding: 20,
+        margin: 20,
+      }}>
+      Hello
     </div>
   );
 }
