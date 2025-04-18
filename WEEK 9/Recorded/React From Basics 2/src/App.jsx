f65cb972c25927d8300ebc4e8f3d1df412538c5c
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-18 09:31:09 +0530

    Fragments

--- WEEK 9/Recorded/React From Basics 2/src/App.jsx
+++ WEEK 9/Recorded/React From Basics 2/src/App.jsx
@@ -2,78 +2,10 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div>
-      <ErrorBoundary>
-        <Card1 />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Card2 />
-      </ErrorBoundary>
-    </div>
-  );
-}
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, info) {
-    console.error("Error caught:", error, info);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div
-          style={{
-            backgroundColor: "red",
-            borderRadius: 20,
-            padding: 20,
-          }}>
-          Something went wrong
-        </div>
-      );
-    }
-
-    return this.props.children;
-  }
-}
-
-function Card1() {
-  // throw new Error("Error while rendering");
-
-  return (
-    <div
-      style={{
-        backgroundColor: "red",
-        borderRadius: 20,
-        padding: 20,
-        marginBottom: 20,
-      }}>
-      Hi there
-    </div>
-  );
-}
-
-function Card2() {
-  // throw new Error("Error while rendering");
-
-  return (
-    <div
-      style={{
-        backgroundColor: "red",
-        borderRadius: 20,
-        padding: 20,
-        margin: 20,
-      }}>
-      Hello
-    </div>
+    <>
+      <div>Hi there</div>
+      <div>Hello</div>
+    </>
   );
 }
 
