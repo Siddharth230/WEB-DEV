11c72e2b7495252b474d4294d50a95978dfea554
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-21 10:43:44 +0530

    WEEK 11

---
+++ WEEK 11/Custom Hooks/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function useCounter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  return {
+    count,
+    increaseCount,
+  };
+}
+
+function App() {
+  const { count, increaseCount } = useCounter();
+  return (
+    <div>
+      <button onClick={increaseCount}>Increase {count}</button>
+    </div>
+  );
+}
+
+export default App;
