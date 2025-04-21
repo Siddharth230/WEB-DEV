b6e79fe961ac4e25c0e76ea9e533163a0ccf607b
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-21 09:02:25 +0530

    Context API

--- WEEK 10/Routing/src/App.jsx
+++ WEEK 10/Routing/src/App.jsx
@@ -1,65 +1,29 @@
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import { useRef, useState } from "react";
 
 function App() {
+  const [currentCount, setCurrentCount] = useState(0);
+  const timer = useRef();
+
+  function startClock() {
+    let value = setInterval(() => {
+      setCurrentCount((c) => c + 1);
+    }, 1000);
+    timer.current = value;
+  }
+
+  function stopClock() {
+    clearInterval(timer.current);
+  }
+
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route
-              path="/neet/online-coaching-class-11"
-              element={<Class11Program />}
-            />
-            <Route
-              path="/neet/online-coaching-class-12"
-              element={<Class12Program />}
-            />
-            <Route path="/" element={<Landing />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-        Footer | Contact us
-      </BrowserRouter>
+      {currentCount}
+      <br />
+      <button onClick={startClock}>Start</button>
+      <button onClick={stopClock}>Stop</button>
+      <button>Reset</button>
     </div>
   );
-}
-
-function Layout() {
-  return (
-    <div style={{ height: "100vh" }}>
-      <Header />
-      <div style={{ height: "90vh" }}>
-        <Outlet />
-      </div>
-      footer
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div>
-      <Link to="/"> Allen </Link> |
-      <Link to="/neet/online-coaching-class-11"> Class 11 </Link> |
-      <Link to="/neet/online-coaching-class-12"> Class 12 </Link>
-    </div>
-  );
-}
-
-function Landing() {
-  return <div>Welcome to Allen</div>;
-}
-
-function Class11Program() {
-  return <div>Neet programs for Class 11th</div>;
-}
-
-function Class12Program() {
-  return <div>Neet programs for Class 12th</div>;
-}
-
-function ErrorPage() {
-  return <div>Sorry page not found</div>;
 }
 
 export default App;
