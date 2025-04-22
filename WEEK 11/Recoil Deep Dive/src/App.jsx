9441bf4ddc2d6e7dd71f4b939fd49f4ea968be25
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-22 20:17:00 +0530

    Recoil Deep Dive

--- WEEK 11/Recoil Deep Dive/src/App.jsx
+++ WEEK 11/Recoil Deep Dive/src/App.jsx
@@ -1,13 +1,20 @@
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { networkAtom } from "./store/atoms/atom";
+
 function App() {
+  const networkNotificationCount = useRecoilValue(networkAtom);
   return (
-    <div>
+    <RecoilRoot>
       <button>Home</button>
-      <button>My network ()</button>
+      <button>
+        My network (
+        {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
+      </button>
       <button>Jobs ()</button>
       <button>Messaging ()</button>
       <button>Notifications ()</button>
       <button>Me</button>
-    </div>
+    </RecoilRoot>
   );
 }
 
