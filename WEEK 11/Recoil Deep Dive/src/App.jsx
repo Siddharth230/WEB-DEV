5f3383527d1f94b06e4135e1cc50b4f47e0c3f73
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-23 12:33:44 +0530

    atomFamily

--- WEEK 11/Recoil Deep Dive/src/App.jsx
+++ WEEK 11/Recoil Deep Dive/src/App.jsx
@@ -1,50 +1,23 @@
 import { RecoilRoot, useRecoilValue } from "recoil";
-import {
-  jobsAtom,
-  messagingAtom,
-  networkAtom,
-  notificationsAtom,
-  totalNotificationSelector,
-} from "./store/atoms/atom";
+import { todosAtomFamily } from "./store/atoms/atom";
 
 function App() {
   return (
     <RecoilRoot>
-      <MainApp />
+      <Todo id={1} />
+      <Todo id={2} />
     </RecoilRoot>
   );
 }
 
-function MainApp() {
-  const networkNotificationCount = useRecoilValue(networkAtom);
-  const jobsNotificationCount = useRecoilValue(jobsAtom);
-  const notificationNotificationCount = useRecoilValue(notificationsAtom);
-  const messagingNotificationCount = useRecoilValue(messagingAtom);
+function Todo({ id }) {
+  const currentTodo = useRecoilValue(todosAtomFamily(id));
 
-  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
   return (
     <>
-      <button>Home</button>
-      <button>
-        My network (
-        {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
-      </button>
-      <button>
-        Jobs ({jobsNotificationCount >= 100 ? "99+" : jobsNotificationCount})
-      </button>
-      <button>
-        Messaging (
-        {messagingNotificationCount >= 100 ? "99+" : messagingNotificationCount}
-        )
-      </button>
-      <button>
-        Notifications (
-        {notificationNotificationCount >= 100
-          ? "99+"
-          : notificationNotificationCount}
-        )
-      </button>
-      <button>Me ({totalNotificationCount})</button>
+      {currentTodo.title}
+      {currentTodo.description}
+      <br />
     </>
   );
 }
--- WEEK 11/Recoil Deep Dive/src/store/atoms/atom.js
+++ WEEK 11/Recoil Deep Dive/src/store/atoms/atom.js
@@ -1,31 +1,9 @@
-import { atom, selector } from "recoil";
+import { atomFamily } from "recoil";
+import { TODOS } from "../../todos";
 
-export const networkAtom = atom({
-  key: "networkAtom",
-  default: 104,
-});
-
-export const jobsAtom = atom({
-  key: "jobsAtom",
-  default: 0,
-});
-export const messagingAtom = atom({
-  key: "messagingAtom",
-  default: 0,
-});
-export const notificationsAtom = atom({
-  key: "notificationsAtom",
-  default: 12,
-});
-
-export const totalNotificationSelector = selector({
-  key: "totalNotificationSelector",
-  get: ({ get }) => {
-    const network = get(networkAtom);
-    const jobs = get(jobsAtom);
-    const messaging = get(messagingAtom);
-    const notifications = get(notificationsAtom);
-
-    return network + jobs + messaging + notifications;
+export const todosAtomFamily = atomFamily({
+  key: "todosAtomFamily",
+  default: (id) => {
+    return TODOS.find((x) => x.id === id);
   },
 });
