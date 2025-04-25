cdc95d4b54a28c75d2111368ec1edc5c89a54126
Siddhath230 <siddharthlokhande04@gmail.com>
2025-04-25 16:33:00 +0530

    WEEK 13

--- WEEK 13/Tailwind/src/App.jsx
+++ WEEK 13/Tailwind/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="h-screen bg-blue-800 text-white">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="flex justify-center p-15">
+          <img src="./../img/camera.svg" />
+          <div className="flex ml-2">
+            <p className="text-blue-300">Webinar</p>.gg
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex justify-center font-bold mb-10">Verify Your Age</div>
+      <div className="flex justify-center text-xs text-gray-400">
+        Please confirm your birth date. This data will not be stored
+      </div>
+      <div className="flex justify-center ">
+        <input
+          className="bg-blue-400 rounded-lg border border-blue-200 px-5 py-2 my-2"
+          type="date"
+          placeholder="Your Birth Year"
+        />
+      </div>
+      <div className="flex justify-center ">
+        <button className="bg-gray-400 px-20 py-2 rounded-lg font-semibold mt-5 cursor-pointer">
+          Continue
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
